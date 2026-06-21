69d320d1f1d47cfc1500ef9382c3ffdb4e59468f
upendramediboina <upendramediboina@gmail.com>
2026-06-21 11:25:26 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,7 +0,0 @@
-//mobile toggle
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
-
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
